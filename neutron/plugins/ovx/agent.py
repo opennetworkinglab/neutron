a28b68368fcb2128b490a18b88d4861d3ad908ff
Marc De Leenheer <marc@onlab.us>
2014-06-24 15:49:33 -0700

    Object field access

--- neutron/plugins/ovx/agent.py
+++ neutron/plugins/ovx/agent.py
@@ -90,9 +90,9 @@ class OVXNeutronAgent():
             # get dpid / port
             # inform agent about port, also pass in device
             ovs_port = self.int_br.get_vif_port_by_id(port)
-            port_id = ovs_port['iface-id']
+            port_id = ovs_port.vif_id
             dpid = self.int_br.get_datapath_id()
-            port_number = ovs_port['ofport']
+            port_number = ovs_port.ofport
             self.plugin_rpc.update_port(self.context, port_id, dpid, port_number)
 
         return resync
