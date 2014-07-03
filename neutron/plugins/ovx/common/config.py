f8395ad99f047b26dfefbe7be404e230da8794ae
Marc De Leenheer <marc@onlab.us>
2014-07-03 16:00:38 -0700

    Explain difference between nova project ID and tenant ID

--- neutron/plugins/ovx/common/config.py
+++ neutron/plugins/ovx/common/config.py
@@ -25,7 +25,7 @@ agent_opts = [
 nova_opts = [
     cfg.StrOpt('username', default='admin', help=_('Nova username.')),
     cfg.StrOpt('password', default='', help=_('Nova password.')),
-    cfg.StrOpt('project_id', default='admin', help=_('Nova project ID.')),
+    cfg.StrOpt('project_id', default='admin', help=_('Nova project ID (name, not the tenant ID given by a UUID).')),
     cfg.StrOpt('auth_url', default='http://localhost:5000/v2.0/', help=_('Nova authentication URL.')),
 ]
 
