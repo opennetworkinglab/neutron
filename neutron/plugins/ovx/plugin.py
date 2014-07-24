f8aa9d0d53d100a3bc5540e6fffda7bb3b4c0398
Marc De Leenheer <marc@onlab.us>
2014-07-24 14:59:02 -0700

    Debug

--- neutron/plugins/ovx/plugin.py
+++ neutron/plugins/ovx/plugin.py
@@ -119,6 +119,7 @@ class ControllerManager():
         controller_id = server.id
         # TODO: need a good way to assign IP address, and obtain it here
         while server.addresses.get(self.ctrl_network_name) == None:
+            LOG.error('WAITING %s' % server.addresses)
             time.sleep(1)
         controller_ip = server.addresses[self.ctrl_network_name][0]['addr']
         #controller_ip = '192.168.56.6'
