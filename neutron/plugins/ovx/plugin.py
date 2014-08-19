93f6e98a0099a24123f4f7bee01608462f7e71bd
Marc De Leenheer <marc@onlab.us>
2014-08-19 14:51:35 -0700

    Novaclient wants the name of the keypair, not the ref

--- neutron/plugins/ovx/plugin.py
+++ neutron/plugins/ovx/plugin.py
@@ -103,10 +103,8 @@ class ControllerManager():
         try:
             self._image = self._nova.images.find(name=cfg.CONF.NOVA.image_name)
             self._flavor = self._nova.flavors.find(name=cfg.CONF.NOVA.flavor)
-            if cfg.CONF.NOVA.key_name == None:
-                self._key_name = None
-            else:
-                self._key_name = self._nova.keypairs.find(name=cfg.CONF.NOVA.key_name)
+            # Check if the key name is found, don't save the ref (novaclient wants the name)
+            self._nova.keypairs.find(name=cfg.CONF.NOVA.key_name)
         except Exception as e:
             LOG.error("Could not initialize Nova bindings. Check your config. %s)" % e)
             sys.exit(1)
@@ -119,7 +117,7 @@ class ControllerManager():
         server = self._nova.servers.create(name='OVX_%s' % name,
                                            image=self._image,
                                            flavor=self._flavor,
-                                           key_name=self._key_name,
+                                           key_name=cfg.CONF.NOVA.key_name,
                                            nics=[nic_config])
         controller_id = server.id
         # TODO: need a good way to obtain IP address
