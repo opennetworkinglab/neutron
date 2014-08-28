51030853d1d4206fdc5f6679ea7319e1fe7f1d35
Marc De Leenheer <marc@onlab.us>
2014-08-28 16:49:51 -0700

    Use temp file for partition table

--- neutron/plugins/ovx/build-vm.sh
+++ neutron/plugins/ovx/build-vm.sh
@@ -34,11 +34,13 @@ fi
 qemu-img resize $IMAGE +500M
 # Sometimes qemu-nbd wants the full path to the image
 sudo qemu-nbd --connect=/dev/nbd0 `pwd`/$IMAGE
-sudo sfdisk -d /dev/nbd0 > s
-sudo sed 's/4192256/4192256/' s
+PTABLE=`mktemp`
+sudo sfdisk -d /dev/nbd0 > $PTABLE
+sed 's/4192256/4192256/' $PTABLE
+sudo sfdisk /dev/nbd0 < $PTABLE
+rm $PTABLE
 sudo e2fsck -f /dev/nbd0p1
 sudo resize2fs /dev/nbd0p1
-rm s
 
 # Mount image and chroot to it
 TMP_DIR=`mktemp -d`
