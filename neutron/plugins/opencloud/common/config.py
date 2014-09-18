b705aa7e609bc8eefc6f1dcd6a8c58fbf73d5f99
Marc De Leenheer <marc@onlab.us>
2014-09-18 11:46:37 -0700

    Fixed inconsistent naming

--- neutron/plugins/opencloud/common/config.py
+++ neutron/plugins/opencloud/common/config.py
@@ -5,6 +5,6 @@ ovs_opts = [
     cfg.StrOpt('ovs_bridge', default='br-nat', help=_("NAT network bridge"))
 ]
 
-cfg.CONF.register_opts(nat_opts, "OVS")
+cfg.CONF.register_opts(ovs_opts, "OVS")
 config.register_agent_state_opts_helper(cfg.CONF)
 config.register_root_helper(cfg.CONF)
