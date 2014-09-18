cc5ed87cf6a1d06775c7539a62350a4c1555f3f0
Marc De Leenheer <marc@onlab.us>
2014-09-18 11:42:43 -0700

    New option group for nat config

--- neutron/plugins/opencloud/common/config.py
+++ neutron/plugins/opencloud/common/config.py
@@ -1,10 +1,10 @@
 from oslo.config import cfg
 from neutron.agent.common import config
 
-ovs_opts = [
+nat_opts = [
     cfg.StrOpt('nat_bridge', default='br-nat', help=_("NAT network bridge"))
 ]
 
-cfg.CONF.register_opts(ovs_opts, "OVS")
+cfg.CONF.register_opts(nat_opts, "NAT")
 config.register_agent_state_opts_helper(cfg.CONF)
 config.register_root_helper(cfg.CONF)
--- neutron/plugins/opencloud/opencloud_neutron_plugin.py
+++ neutron/plugins/opencloud/opencloud_neutron_plugin.py
@@ -91,7 +91,7 @@ class OpenCloudPluginV2(OVXNeutronPlugin):
         """Set port binding to NAT bridge."""
         if port['port']['network_id'] == self.nat_network['id']:
             LOG.debug("Setting port binding to nat for port %s" % port['port'])
-            self.base_binding_dict[portbindings.BRIDGE] = cfg.CONF.OVS.nat_bridge
+            self.base_binding_dict[portbindings.BRIDGE] = cfg.CONF.NAT.nat_bridge
             
         return super(OpenCloudPluginV2, self).create_port(context, port)
         
