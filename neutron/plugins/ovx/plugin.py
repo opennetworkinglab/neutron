d4969fdfc29e562b6d04c6c1e12c615b76293b24
Marc De Leenheer <marc@onlab.us>
2014-09-19 15:53:07 -0700

    Ref to full port info

--- neutron/plugins/ovx/plugin.py
+++ neutron/plugins/ovx/plugin.py
@@ -382,7 +382,7 @@ class OVXNeutronPlugin(db_base_plugin_v2.NeutronDbPluginV2,
             db_state = port_db['admin_state_up']
         
             # Start or stop port in OVX (data ports only!) as requested
-            if port['network_id'] == self.ctrl_network['id']:
+            if port_db['network_id'] == self.ctrl_network['id']:
                 if (req_state != None) and (req_state != db_state):
                     ovx_tenant_id = ovxdb.get_ovx_network(context.session, port_db['network_id']).ovx_tenant_id
                     ovx_port = ovxdb.get_ovx_port(context.session, id)
